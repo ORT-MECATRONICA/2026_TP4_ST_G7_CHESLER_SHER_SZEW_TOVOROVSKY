01c8fbcef816089eec6f9f7bd4e18dd2f1098b77
AleSzew <alejandroszew@gmail.com>
2026-06-18 09:12:46 -0300

    termine el ejercicio, queda probarlo

---
+++ Ejercicio1/Ejercicio1.ino
@@ -0,0 +1,121 @@
+#include <U8g2lib.h>
+#include <DHT.h>
+#include <Preferences.h>
+#include <Ticker.h>
+
+// OBJETOS
+Preferences preferences;  // memoria no volátil
+Ticker tickerBoton;       // temporizador para contar segundos
+
+// SENSOR Y OLED
+#define DHTPIN 23
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+
+// BOTONES
+#define sw1 35
+#define sw2 34
+
+// ESTADOS
+#define PANTALLA_1 1
+#define ESPERAR_SW1 2
+#define PANTALLA_2 3
+#define ESPERAR_SW2 4
+#define AUMENTAR_UMBRAL 5
+#define DISMINUIR_UMBRAL 6
+
+int estado = PANTALLA_1;
+
+// VARIABLES
+float temperatura = 0;
+int umbral = 26;
+int segundosBoton = 0;  // contador de segundos
+
+// --- FUNCIONES AUXILIARES ---
+void tiempoBotones() {
+  segundosBoton++;
+}
+
+void guardarUmbral() {
+}
+
+void mostrarPantalla1() {
+
+
+  void setup() {
+    Serial.begin(115200);
+    pinMode(sw1, INPUT_PULLUP);
+    pinMode(sw2, INPUT_PULLUP);
+    dht.begin();
+    u8g2.begin();
+
+    // Ticker cada 1s para contar segundos
+    tickerBoton.attach(1, tiempoBotones);
+
+    // Leer umbral guardado
+    preferences.begin("my-app", false);
+    umbral = preferences.getInt("umbral", 26);
+    preferences.end();
+  }
+
+  void loop() {
+    temperatura = dht.readTemperature();
+    switch (estado) {
+      case PANTALLA_1:
+        u8g2.clearBuffer();
+        u8g2.setFont(u8g2_font_ncenB10_tr);
+        u8g2.drawStr(5, 15, "VA:");
+        char buf1[10];
+        sprintf(buf1, "%.1f C", temperatura);
+        u8g2.drawStr(50, 15, buf1);
+        u8g2.drawStr(5, 35, "VU:");
+        char buf2[10];
+        sprintf(buf2, "%d C", umbral);
+        u8g2.drawStr(50, 35, buf2);
+        u8g2.sendBuffer();
+        if (digitalRead(sw1) == LOW) {
+          segundosBoton = 0;
+          estado = ESPERAR_SW1;
+        }
+        break;
+
+      case ESPERAR_SW1:
+        if (digitalRead(sw1) == HIGH) estado = PANTALLA_1;
+        else if (segundosBoton >= 5) estado = PANTALLA_2;
+        break;
+
+      case PANTALLA_2:
+        u8g2.clearBuffer();
+        u8g2.setFont(u8g2_font_ncenB10_tr);
+        u8g2.drawStr(5, 20, "VU:");
+        char buf3[10];
+        sprintf(buf3, "%d C", umbral);
+        u8g2.drawStr(50, 20, buf3);
+        u8g2.sendBuffer();
+        if (digitalRead(sw1) == LOW) estado = AUMENTAR_UMBRAL;
+        else if (digitalRead(sw2) == LOW) estado = DISMINUIR_UMBRAL;
+        break;
+
+      case AUMENTAR_UMBRAL:
+        if (digitalRead(sw1) == HIGH) {
+          umbral++;
+          estado = PANTALLA_2;
+        } 
+        }
+        break;
+
+      case DISMINUIR_UMBRAL:
+        if (digitalRead(sw2) == HIGH) {
+          umbral--;
+          estado = PANTALLA_2;
+        } else if (segundosBoton >= 5) {
+          estado = PANTALLA_1;
+          preferences.begin("my-app", false);
+          preferences.putInt("umbral", umbral);
+          preferences.end();
+          Serial.println("Umbral guardado en memoria");  // solo guarda al volver a P1
+        }
+        break;
+    }
+  }
